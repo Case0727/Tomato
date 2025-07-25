f0cff821e2655d652a8fa71028c6c6205b76332d
Chinmay Satam <133474970+Case0727@users.noreply.github.com>
2025-07-25 19:07:56 +0530

    Update AdminApp.jsx

--- frontend/src/pages/admin/AdminApp.jsx
+++ frontend/src/pages/admin/AdminApp.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 import './AdminApp.css'
 
 export default function AdminApp() {
-  const url = 'https://tomato-82zz.onrender.com'
+  const url = 'https://tomato-backend-a0u9.onrender.com'
   return (
     <>
       <div>
